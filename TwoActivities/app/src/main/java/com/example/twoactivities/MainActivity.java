b0c83ffcb1c6db73ff3c8a81e22a01f9489830e2
Prashant <prasbas97@gmail.com>
2021-02-19 23:49:12 +0545

    Completed Tasks of TwoActivites

--- TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.twoactivities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,11 +8,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
     private EditText mMessageEditText;
+    public static final int TEXT_REQUEST = 1;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
 
 
     @Override
@@ -20,14 +25,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mMessageEditText = findViewById(R.id.editText_main);
 
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+
     }
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(this, SecondActivity.class);
         String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        //startActivity(intent);
+        Log.d(LOG_TAG, ""+ message);
 
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        startActivityForResult(intent,TEXT_REQUEST);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
--- TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -4,18 +4,83 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+
+    /**
+     * defining key for the Intent extra
+     * public constant
+     */
+    public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
+
+    /**
+     * holds EditText
+     */
+    private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        /**
+         * reference to the EditText in SecondActivity
+         */
+
+        mReply = findViewById(R.id.editText_second);
+
+        /**
+         * coding of Intent
+         */
+
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+
         TextView textView = findViewById(R.id.text_message);
+
         textView.setText(message);
+
+    }
+
+    /**
+     * for the response
+     * @param view
+     */
+
+    public void returnReply(View view) {
+
+        String reply = mReply.getText().toString();
+
+        /**
+         * new intent object for reply
+         */
+
+        Intent replyIntent = new Intent();
+
+        /**
+         * key value pairs
+         * key=EXTRA_REPLY
+         * value=reply
+         */
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+
+        /**
+         * for success response
+         */
+        setResult(RESULT_OK,replyIntent);
+
+        /**
+         * closes the activity
+         */
+        finish();
+
+
+
 
     }
 }
